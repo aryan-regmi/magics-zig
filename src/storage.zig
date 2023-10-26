6ab8ae3ef02363cbe38407ed93e0e7dc84d6fccf
Aryan Regmi <aryan_regmi@protonmail.com>
2023-10-26 16:50:36 -0600

    Added sparse storage

---
+++ src/storage.zig
@@ -0,0 +1,100 @@
+const std = @import("std");
+const world = @import("./world.zig");
+const Allocator = std.mem.Allocator;
+const ArrayListUnmanaged = std.ArrayListUnmanaged;
+const Entity = world.Entity;
+
+// TODO: Add bound checks
+
+/// Determines the type of storage for a component type.
+///
+/// `Sparse` storage is optimized for fast insertions and deletions, but
+/// iterating over the components will be slower.
+/// `Dense` storage is optimized for fast iterations, but insertions and
+/// deletions will be slower.
+///
+/// If you are unsure what type of storage to use for a component type, default
+/// it to to `Dense`, and change it only if you notice significant difference in
+/// performance.
+pub const StorageType = enum { Sparse, Dense };
+
+/// Storage for a single type of component, where the entity data is stored sparsely.
+pub fn SparseComponentStorage(comptime Component: type) type {
+    return struct {
+        const Self = @This();
+
+        /// The total number of entities with this component type.
+        num_entities: usize,
+
+        /// The sparsely stored component data.
+        data: ArrayListUnmanaged(?Component) = .{},
+
+        /// Create new `SparseComponentStorage`.
+        pub fn init(allocator: *const Allocator, num_entities: usize) !Self {
+            // Create new array list and initalize values to `null`
+            var data = try ArrayListUnmanaged(?Component).initCapacity(allocator.*, num_entities);
+            for (0..num_entities) |_| {
+                try data.append(allocator.*, null);
+            }
+
+            return .{
+                .num_entities = num_entities,
+                .data = data,
+            };
+        }
+
+        /// Frees memory used by the storage.
+        pub fn deinit(self: *Self, allocator: Allocator) void {
+            self.data.deinit(allocator);
+            self.num_entities = 0;
+        }
+    };
+}
+
+/// A type erased `SparseComponentStorage` for storage in the world.
+pub const ErasedSparseStorage = struct {
+    const Self = @This();
+
+    /// Points the the concrete `SparseComponentStorage`.
+    ptr: *anyopaque,
+
+    /// Function to add an empty entry in the storage.
+    addEmptyEntity: *const fn (self: *Self, allocator: Allocator) anyerror!void,
+
+    /// Frees memory used by the storage.
+    deinit: *const fn (self: *Self, allocator: Allocator) void,
+
+    /// Initialize a new sparsed storage, return the erased storage pointing at it.
+    pub fn init(comptime Component: type, allocator: *const Allocator, num_entities: usize) !Self {
+        var ptr = try allocator.create(SparseComponentStorage(Component));
+        ptr.* = try SparseComponentStorage(Component).init(allocator, num_entities);
+
+        return .{
+            .ptr = ptr,
+            .addEmptyEntity = (struct {
+                pub fn addEmptyEntity(self: *Self, allocator_: Allocator) anyerror!void {
+                    var concrete = Self.toConcrete(self.ptr, Component);
+                    try concrete.data.append(allocator_, null);
+                }
+            }).addEmptyEntity,
+            .deinit = (struct {
+                pub fn deinit(self: *Self, allocator_: Allocator) void {
+                    var concrete = Self.toConcrete(self.ptr, Component);
+                    concrete.deinit(allocator_);
+                    allocator_.destroy(concrete);
+                }
+            }).deinit,
+        };
+    }
+
+    /// Casts the erased sparse storage to its concrete type.
+    pub fn toConcrete(ptr: *anyopaque, comptime Component: type) *SparseComponentStorage(Component) {
+        return @ptrCast(@alignCast(ptr));
+    }
+
+    /// Updates the component value for to `component` fo the given entity.
+    pub fn updateValue(self: *Self, comptime Component: type, entity: Entity, component: Component) void {
+        var concrete = Self.toConcrete(self.ptr, Component);
+        concrete.data.items[entity] = component;
+    }
+};
---
+++ src/world.zig
@@ -0,0 +1,126 @@
+const std = @import("std");
+const storage = @import("./storage.zig");
+const Allocator = std.mem.Allocator;
+const Hashmap = std.AutoArrayHashMapUnmanaged;
+const ErasedSparseStorage = storage.ErasedSparseStorage;
+const SparseStorage = storage.SparseComponentStorage;
+
+// NOTE: All `Component` types must have a `storage::StorageType` defined to be either `.Sparse` or `.Dense`.
+
+/// Represents an entity in the world.
+pub const Entity = usize;
+
+/// The hash of a component type.
+pub const ComponentHash = u64;
+
+/// The world that contains all entities and their components in the ECS.
+pub const World = struct {
+    const Self = @This();
+
+    allocator: Allocator,
+
+    /// The total number of entities in the world.
+    total_entities: usize = 0,
+
+    sparse_components: Hashmap(ComponentHash, ErasedSparseStorage) = .{},
+
+    /// Creates new `World`.
+    pub fn init(allocator: Allocator) Self {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    /// Destroys the world and frees any allocations it made.
+    pub fn deinit(self: *Self) void {
+        // Free all sparse components
+        for (self.sparse_components.keys()) |component_type| {
+            var erased_storage: *ErasedSparseStorage = self.sparse_components.getPtr(component_type).?;
+            erased_storage.deinit(erased_storage, self.allocator);
+        }
+        self.sparse_components.deinit(self.allocator);
+    }
+
+    /// Spawns an empty entity.
+    pub fn spawn(self: *Self) !Entity {
+        const entity = self.total_entities;
+        self.total_entities += 1;
+
+        // Add empty entry to all sparse components!
+        for (self.sparse_components.keys()) |component_type| {
+            var erased_storage: *ErasedSparseStorage = self.sparse_components.getPtr(component_type).?;
+            try erased_storage.addEmptyEntity(erased_storage, self.allocator);
+        }
+
+        return entity;
+    }
+
+    /// Adds the specified component the the entity.
+    pub fn addComponentToEntity(self: *Self, entity: Entity, component: anytype) !void {
+        const ComponentType = @TypeOf(component);
+        const COMPONENT_HASH = std.hash_map.hashString(@typeName(ComponentType));
+
+        comptime {
+            if (!@hasDecl(ComponentType, "StorageType")) {
+                @compileError("The component type must have a `const StorageType: storage.StorageType` field");
+            }
+        }
+
+        if (ComponentType.StorageType == storage.StorageType.Sparse) {
+            // If component type exists already, then just update the existing value
+            if (self.sparse_components.contains(COMPONENT_HASH)) {
+                // Update the component value
+                var erased_storage: *ErasedSparseStorage = self.sparse_components.getPtr(COMPONENT_HASH).?;
+                erased_storage.updateValue(ComponentType, entity, component);
+                return;
+            }
+
+            // Create new component type storage
+            var new_storage = try ErasedSparseStorage.init(ComponentType, &self.allocator, self.total_entities);
+            new_storage.updateValue(ComponentType, entity, component);
+            try self.sparse_components.put(self.allocator, COMPONENT_HASH, new_storage);
+        }
+
+        // TODO: Add logic for dense/archetype storage
+    }
+
+    // pub fn getComponentForEntity(self: *Self, entity: Entity)
+};
+
+test "Can create world" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    var world = World.init(allocator);
+    defer world.deinit();
+}
+
+test "Can spawn empty entity" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    var world = World.init(allocator);
+    defer world.deinit();
+
+    const e0 = try world.spawn();
+    try testing.expectEqual(e0, 0);
+
+    const e1 = try world.spawn();
+    try testing.expectEqual(e1, 1);
+}
+
+test "Can add component to entity" {
+    const testing = std.testing;
+    const allocator = testing.allocator;
+
+    const TestStruct = struct {
+        const StorageType: storage.StorageType = .Sparse;
+        val: i32,
+    };
+
+    var world = World.init(allocator);
+    defer world.deinit();
+
+    var entity = try world.spawn();
+    try world.addComponentToEntity(entity, TestStruct{ .val = 42 });
+}
